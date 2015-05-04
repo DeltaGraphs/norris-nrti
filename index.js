4733a566b5d1412cde79d9e0f5a93b82e0a193f5
matfur92 <mat_fur_92_pub@yahoo.it>
2015-05-04 18:03:10 +0200

    test automatic deploy

--- index.js
+++ index.js
@@ -2,3 +2,27 @@
 'use strict';
 
 console.log('Hello World');
+
+var http = require('http');
+
+http.createServer(function (req, res) {
+  var html = buildHtml(req);
+
+  res.writeHead(200, {
+    'Content-Type': 'text/html',
+    'Content-Length': html.length,
+    'Expires': new Date().toUTCString()
+  });
+  res.end(html);
+}).listen(80);
+
+function buildHtml(req) {
+  var header = '<title>Hello World</title>';
+  var body = '<p>Hello World</p>';
+
+  // concatenate header string
+  // concatenate body string
+
+  return '<!DOCTYPE html>'
+       + '<html><header>' + header + '</header><body>' + body + '</body></html>';
+};
