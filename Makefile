1a9c8713f082ea6c43f2743f000da32c1c3ecbbc
MatFur92 <mat_fur_92_pub@yahoo.it>
2015-05-09 09:24:07 +0200

    add module.export

--- Makefile
+++ Makefile
@@ -11,4 +11,4 @@ default: test
 test:
 	@echo "$(PROJECT)Executing JSHint..."
 	@$(JSHINT) $(JSHINT_OPTS) $(JS_FILES)
-	istanbul cover ./node_modules/mocha/bin/_mocha --report lcovonly -- -R spec && cat ./coverage/lcov.info | ./node_modules/coveralls/bin/coveralls.js && rm -rf ./coverage
+	
--- norris-nrti.js
+++ norris-nrti.js
@@ -1,22 +1,24 @@
 /*jshint node: true, -W106 */
 'use strict';
 
-console.log('Hello World');
+module.exports = function() {
+	console.log('Hello World');
 
-var http = require('http');
-var port = process.env.PORT || 3000;
+	var http = require('http');
+	var port = process.env.PORT || 3000;
 
-http.createServer(function (req, res) {
-  var html = buildHtml();
+	http.createServer(function (req, res) {
+	  var html = buildHtml();
 
-  res.writeHead(200, {
-    'Content-Type': 'text/html',
-    'Content-Length': html.length,
-    'Expires': new Date().toUTCString()
-  });
-  res.end(html);
-}).listen(port);
+	  res.writeHead(200, {
+	    'Content-Type': 'text/html',
+	    'Content-Length': html.length,
+	    'Expires': new Date().toUTCString()
+	  });
+	  res.end(html);
+	}).listen(port);
 
-function buildHtml() {
-	return '<!DOCTYPE html><html><header><title>Hello World</title></header><body><p>Hello World</p></body></html>';
+	function buildHtml() {
+		return '<!DOCTYPE html><html><header><title>Hello World</title></header><body><p>Hello World</p></body></html>';
+	}
 }
