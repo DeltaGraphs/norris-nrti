4b614a10f67387671c6a9b735264898b582d2803
MatFur92 <mat_fur_92_pub@yahoo.it>
2015-05-12 09:15:46 +0200

    add echo to debug travis script

--- clone-gh-pages.sh
+++ clone-gh-pages.sh
@@ -1,15 +1,18 @@
-echo "prima if"
+echo "Script update-gh-pages..."
+pwd
+echo "$TRAVIS_PULL_REQUEST"
 if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
   echo -e "Starting to cloning gh-pages\n"
 
   cd $HOME
-  
+  pwd
   #go to home and setup git
   git config --global user.email "deltagraphs@gmail.com"
   git config --global user.name "deltagraphsManager"
   echo "prima clone"
   #using token clone gh-pages branch
-  git clone --branch=gh-pages https://${GH_TOKEN}@github.com/DeltaGraphs/norris-nrti.git  gh-pages > /dev/null
+  git clone --branch=gh-pages https://${GH_TOKEN}@github.com/DeltaGraphs/norris-nrti.git  gh-pages
 
   echo -e "Cloned gh-pages\n"
 fi
+echo "Fine script update-gh-pages..."
