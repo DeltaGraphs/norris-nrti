bcc14e4584337812f9b36c02c5ba3a90dbf0a657
MatFur92 <mat_fur_92_pub@yahoo.it>
2015-05-13 12:49:27 +0200

    add test to graph package

---
+++ test/dataTier/graph/barChartModelSpec.js
@@ -0,0 +1,23 @@
+/*jshint node: true, -W106 */
+'use strict';
+
+/*
+* Name : barChartModelSpec.js
+* Module : UnitTest
+* Location : /test/dataTier/graph
+*
+* History :
+* 
+* Version   Date         Programmer         Description
+* =========================================================
+* 0.0.1     2015-05-13   Matteo Furlan    Initial code
+* =========================================================
+*/
+
+var PageListModel = require('../../../lib/dataTier/graph/barChartModel.js');
+//var assert = require('chai').assert;
+var assert = require('assert');
+
+describe('PageListModel', function() {
+    
+});
---
+++ test/dataTier/graph/graphModelSpec.js
@@ -0,0 +1,30 @@
+/*jshint node: true, -W106 */
+'use strict';
+
+/*
+* Name : graphModelSpec.js
+* Module : UnitTest
+* Location : /test/dataTier/graph
+*
+* History :
+* 
+* Version   Date         Programmer         Description
+* =========================================================
+* 0.0.1     2015-05-13   Matteo Furlan    Initial code
+* =========================================================
+*/
+
+var GraphModel = require('../../../lib/dataTier/graph/graphModel.js');
+//var assert = require('chai').assert;
+var assert = require('assert');
+
+describe('GraphModel', function() {
+    it('returns null when passed a non-valid string', function() {
+        assert.strictEqual((new GraphModel({ID:new Date()})).hasOwnProperty('_ID'), false);
+        assert.strictEqual((new GraphModel({ID:''})).hasOwnProperty('_ID'), false);
+    });
+    it('set default values when have wrong type', function() {
+        assert.strictEqual((new GraphModel({ID:'grafico'}))._title, '');
+        assert.strictEqual((new GraphModel({ID:'grafico'}))._enableLegend, '');
+    });
+});
