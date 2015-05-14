19f11157fd95e9e8d0f764c701ac67e0df6d21ea
Filippo <filippo.pado@gmail.com>
2015-05-14 23:12:16 +0200

    improve test

--- test/dataTier/pageList/pageListModelSpec.js
+++ test/dataTier/pageList/pageListModelSpec.js
@@ -15,6 +15,7 @@
 */
 
 var PageListModel = require('../../../lib/dataTier/pageList/pageListModel.js');
+var PageModel = require('../../../lib/dataTier/page/pageModel.js');
 var assert = require('assert');
 
 describe('PageListModel', function() {
@@ -30,61 +31,49 @@ describe('PageListModel', function() {
         assert.strictEqual((new PageListModel('name'))._name, 'name');
     });
 
-    function PageModel(prop, data){
-        this.getData=function(){
-            return data;
-        };
-        this.getProperties=function(){
-            return prop;
-        };
-    }
-
-    var page1=new PageModel('testProp1', 'testData1');
-    var page2=new PageModel('testProp2', 'testData2');
+    var page1=new PageModel('page1');
+    var page2=new PageModel('page2');
 
     describe('#addPage', function() {
         it('returns true and pushes the page if page is valid', function() {
-            var pageList1=new PageListModel('name');
-            assert.strictEqual(pageList1.addPage(page1), true);
-            assert.strictEqual(pageList1._pages.length, 1);
+            var pageList=new PageListModel('name');
+            assert.strictEqual(pageList.addPage(page1), true);
+            assert.strictEqual(pageList._pages.length, 1);
         });
         it('returns false if page is invalid', function() {
-            var pageList1=new PageListModel('name');
-            assert.strictEqual(pageList1.addPage(2), false);
+            var pageList=new PageListModel('name');
+            assert.strictEqual(pageList.addPage(2), 511);
         });
     });
 
     describe('#getName', function() {
         it('returns the right name', function() {
-            var pageList1=new PageListModel('name');
-            assert.strictEqual(pageList1.getName(), 'name');
+            var pageList=new PageListModel('name');
+            assert.strictEqual(pageList.getName(), 'name');
         });
     });
 
     describe('#getData', function() {
         it('returns empty json if it has no pages', function() {
-            var pageList2=new PageListModel('name2');
-            assert.strictEqual(pageList2.getData().length, 0);
+            var pageList=new PageListModel('name');
+            assert.strictEqual(pageList.getData().length, 0);
         });
        
         it('returns json with one page', function() {
-            var pageList2=new PageListModel('name2');
-            pageList2.addPage(page1);
-            var data=pageList2.getData();
+            var pageList=new PageListModel('name');
+            pageList.addPage(page1);
+            var data=pageList.getData();
             assert.strictEqual(data.length, 1);
-            assert.strictEqual(data[0].properties, 'testProp1');
-            assert.strictEqual(data[0].data, 'testData1');
+            assert.strictEqual(data[0].properties.ID, 'page1');
         });
         it('returns json with two pages', function() {
-            var pageList2=new PageListModel('name2');
-            pageList2.addPage(page1);
-            pageList2.addPage(page2);
-            var data=pageList2.getData();
+            var pageList=new PageListModel('name');
+            pageList.addPage(page1);
+            pageList.addPage(page2);
+            var data=pageList.getData();
             assert.strictEqual(data.length, 2);
-            assert.strictEqual(data[0].properties, 'testProp1');
-            assert.strictEqual(data[0].data, 'testData1');
-            assert.strictEqual(data[1].properties, 'testProp2');
-            assert.strictEqual(data[1].data, 'testData2');
+            assert.strictEqual(data[0].properties.ID, 'page1');
+            assert.strictEqual(data[1].properties.ID, 'page2');
         });
     });
     
