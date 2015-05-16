4f376e517de116ac926a1588ac8d17fb984134a7
MatFur92 <mat_fur_92_pub@yahoo.it>
2015-05-16 22:30:10 +0200

    move to startBuildFrontend install scripts

--- frontend/startBuildFrontend.sh
+++ frontend/startBuildFrontend.sh
@@ -1,1 +1,15 @@
+npm install bower
+bower cache clean && bower install
+npm install jshint
+npm plato
+npm complexity-report
+npm karma --save-dev
+npm karma-jasmine
+npm karma-junit-reporter
+npm karma-phantomjs-launcher
+npm karma-coverage
+npm karma-chrome-launcher
+npm karma-firefox-launcher
+npm karma-html2js-preprocessor
+npm karma-script-launcher
 make test --always-make
