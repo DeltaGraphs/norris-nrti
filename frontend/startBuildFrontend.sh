f391122ea5837ff6afde0b74f9020edaf2d2b877
MatFur92 <mat_fur_92_pub@yahoo.it>
2015-05-16 22:33:09 +0200

    fix npm install

--- frontend/startBuildFrontend.sh
+++ frontend/startBuildFrontend.sh
@@ -1,15 +1,15 @@
 npm install bower
 bower cache clean && bower install
 npm install jshint
-npm plato
-npm complexity-report
-npm karma --save-dev
-npm karma-jasmine
-npm karma-junit-reporter
-npm karma-phantomjs-launcher
-npm karma-coverage
-npm karma-chrome-launcher
-npm karma-firefox-launcher
-npm karma-html2js-preprocessor
-npm karma-script-launcher
+npm install plato
+npm install complexity-report
+npm install karma --save-dev
+npm install karma-jasmine
+npm install karma-junit-reporter
+npm install karma-phantomjs-launcher
+npm install karma-coverage
+npm install karma-chrome-launcher
+npm install karma-firefox-launcher
+npm install karma-html2js-preprocessor
+npm install karma-script-launcher
 make test --always-make
