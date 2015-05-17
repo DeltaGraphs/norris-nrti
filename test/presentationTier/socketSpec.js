768aa604cca326d511bb614c248aa6b7af5f4298
Samuele Zanella <baioke@live.it>
2015-05-17 22:45:24 +0200

    add socket test

--- test/presentationTier/socketSpec.js
+++ test/presentationTier/socketSpec.js
@@ -19,9 +19,35 @@ var assert = require('assert');
 
 describe('Socket', function() {
 
-    var nsp = require('socket.io')().of('/namespace');
     it('returns null when there are no params', function() {
+        assert.strictEqual((new Socket()).hasOwnProperty('_namespace'), false);
+    });
+
+    it('returns null when there is a non-valid namespace in params', function() {
+        assert.strictEqual((new Socket('wrong')).hasOwnProperty('_title'), false);
+    });
+
+    var nsp = require('socket.io')().of('/namespace');
+    it('set param values to properties', function() {
         var socket1 = new Socket(nsp);
         assert.strictEqual(socket1._namespace, nsp);
+        assert.strictEqual(socket1._attachedObj, null);
+    });
+
+    function RandomObj() {
+        this.prop1 = 'prop1';
+        this.prop2 = 'prop2';
+    }
+    describe('#attachObject', function() {
+        it('does nothing if there are no params', function() {
+            var socket1 = new Socket(nsp);
+            socket1.attachObject();
+            assert.strictEqual(socket1._attachedObj, null);
+        });
+        it('attach the object in params', function() {
+            var socket1 = new Socket(nsp);
+            socket1.attachObject(new RandomObj());
+            assert.strictEqual(socket1._attachedObj, null);
+        });
     });
 });
