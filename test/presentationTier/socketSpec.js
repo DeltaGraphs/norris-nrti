f7a39605220abe0bf819e61949a5303c431c42f4
Samuele Zanella <baioke@live.it>
2015-05-17 15:08:25 +0200

    add Socket test

---
+++ test/presentationTier/socketSpec.js
@@ -0,0 +1,27 @@
+/*jshint node: true, -W106 */
+'use strict';
+
+/*
+* Name : socketSpec.js
+* Module : UnitTest
+* Location : /test/presentationTier
+*
+* History :
+* 
+* Version   Date         Programmer         Description
+* =========================================================
+* 0.0.1     2015-05-17   Samuele Zanella    Initial code
+* =========================================================
+*/
+
+var Socket = require('../../../lib/dataTier/graph/socket.js');
+var assert = require('assert');
+
+describe('Socket', function() {
+
+    var nsp = require('socket.io')().of('/namespace');
+    it('returns null when there are no params', function() {
+        var socket1 = new Socket(nsp);
+        assert.strictEqual((new Socket())._namespace, nsp);
+    });
+});
