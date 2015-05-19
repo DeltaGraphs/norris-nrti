0d8810673834539bc512a078ca47d5bfdce0c22f
Maria Giovanna Chinellato <mariagiovanna.chinellato1993@gmail.com>
2015-05-19 15:53:59 +0200

    some fixes

--- frontend/app/Model/GraphsModel/BarChart.js
+++ frontend/app/Model/GraphsModel/BarChart.js
@@ -147,8 +147,8 @@ angular.module('services')
                 for (var j=0; j<flowList.length; j++) {
                     if (flowList[j].id === newData[i].ID) {
                         console.log('flowlist[j].id ' + flowList[j].id);
-                        console.log('low list stampa ' + flowList[j]);
                         flowList[j].initializeData(newData[i]);
+                        console.log('flow list stampa ' + flowList[j].toString());
                     }
                 }
             }
