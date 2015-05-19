d368817e91c512560c506d919424385f23955458
Maria Giovanna Chinellato <mariagiovanna.chinellato1993@gmail.com>
2015-05-19 16:29:19 +0200

    some fixes

--- frontend/test/unit/Model/BarChart.js
+++ frontend/test/unit/Model/BarChart.js
@@ -273,7 +273,6 @@ describe('BarChart', function(){
 			BarChart = new BarChart();
 			newFlow = new BarChartFlow({});
 			BarChart.addFlow(data[0].ID, newFlow);
-			BarChart.inPlaceUpdate(data1);
 		});
 
 		afterEach(function(){
@@ -281,7 +280,8 @@ describe('BarChart', function(){
 		});	
 
 		it('BarChartFlow inPlaceUpdate called in the right way', function(){
-			//expect(BarChart.parent.getFlowList()[0].getData()[0].value).toEqual([3,3]);
+			expect(BarChart.parent.getFlowList()[0].getData()[0].value).toEqual([3,3]);
+			BarChart.inPlaceUpdate(data1);
 			expect(BarChart.getFlowList()[0].getData()[0].value).toEqual([4,4]);
 		});
 	});
