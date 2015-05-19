440bad18e4e8a74bbcbbb818ff4f1a5723cd8303
Samuele Zanella <baioke@live.it>
2015-05-19 23:50:37 +0200

    edit routes

---
+++ test/presentationTier/routesSpec.js
@@ -0,0 +1,28 @@
+/*jshint node: true, -W106 */
+'use strict';
+
+/*
+* Name : routesSpec.js
+* Module : UnitTest
+* Location : /test/presentationTier
+*
+* History :
+* 
+* Version   Date         Programmer         Description
+* =========================================================
+* 0.0.1     2015-05-19   Samuele Zanella    Initial code
+* =========================================================
+*/
+
+var Routes = require('../../lib/presentationTier/routes.js');
+
+var assert = require('assert');
+var Express = require('express');
+
+describe('Routes', function() {
+    var expr = new Express();
+    console.dir(expr.prototype.constructor);
+    it('returns null when there are no params', function() {
+        assert.strictEqual((new Routes()).hasOwnProperty('_app'), false);
+    });
+});
