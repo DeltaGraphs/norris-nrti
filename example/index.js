bec46c69a9283c5719336220471907f0b4ae533d
MatFur92 <mat_fur_92_pub@yahoo.it>
2015-05-20 12:18:49 +0200

    fix path + star example norris

--- example/index.js
+++ example/index.js
@@ -1,22 +1,31 @@
 /*jshint node: true, -W106 */
 'use strict';
 
-console.log('Hello World');
+console.log('Hello Norris');
 
+var express = require('express');
+var app = express();
 var http = require('http');
-var port = process.env.PORT || 3000;
+var server = http.createServer(app);
+var io = require('socket.io')(server);
 
-http.createServer(function (req, res) {
-	var html = buildHtml();
+// Includo e creo l'ustanza di Norris
+var Norris = require('../norris-nrti.js');
+var norris = new Norris(app,io,'/norris');
 
-	res.writeHead(200, {
-		'Content-Type': 'text/html',
-		'Content-Length': html.length,
-		'Expires': new Date().toUTCString()
-	});
-	res.end(html);
-}).listen(port);
+console.log(norris);
 
-function buildHtml() {
-	return '<!DOCTYPE html><html><header><title>Hello World</title></header><body><p>Hello World</p></body></html>';
-}
+app.get('/', function (req, res) {
+	var fullUrl = req.protocol + '://' + req.get('host') + req.originalUrl;
+	console.log(' HOME APS - expressEsempio.js '+ fullUrl);
+	res.send(' HOME APS - expressEsempio.js '+ fullUrl);
+});
+
+// questo middleware non permetterà di raggiungere altre richieste scritte al di sotto di questa funzione
+// utilizzabile come 404
+app.use(function(req, res, next) {
+	res.status(404).send('404 Not Found');
+});
+
+//app.listen(3000); // si mette in ascolto su http://127.0.0.1:3000/
+server.listen(3000);
