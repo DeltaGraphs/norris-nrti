1c7447ac4b99725e0f633b2161766f12a0a41aed
Filippo <filippo.pado@gmail.com>
2015-06-02 16:56:37 +0200

    fix mock

--- test/businessTier/flow/mapChartFlowSpec.js
+++ test/businessTier/flow/mapChartFlowSpec.js
@@ -18,7 +18,10 @@ var MapChartFlow = require('../../../lib/businessTier/flow/mapChartFlow.js');
 var assert = require('assert');
 
 describe('MapChartFlow', function() {
-    
+    var socketMock={
+        _namespace: 2,
+        sendMessage: function(){}
+    };
     it('returns null when there are no params', function() {
         assert.strictEqual((new MapChartFlow()).hasOwnProperty('_dataMapChartFlow'), false);
     });
@@ -28,22 +31,22 @@ describe('MapChartFlow', function() {
     });
 
     it('returns null when params are not valid', function() {
-        assert.strictEqual((new MapChartFlow(2, {_namespace: 2})).hasOwnProperty('_dataMapChartFlow'), false);
+        assert.strictEqual((new MapChartFlow(2, socketMock)).hasOwnProperty('_dataMapChartFlow'), false);
     });
 
     it('calls the parent constructor with the params specified', function() {
-        var flow1=new MapChartFlow({ID: 'flow1'},{_namespace: 'flow1'});
+        var flow1=new MapChartFlow({ID: 'flow1'},socketMock);
         assert.strictEqual(flow1._dataMapChartFlow._ID, 'flow1');
         assert.strictEqual(flow1._graphSocket._namespace, 'flow1');
     });
 
     describe('#addRecord', function() {
         it('returns 133 if record is invalid', function() {
-            var flow1=new MapChartFlow({ID: 'flow1'},{_namespace: 'flow1'});
+            var flow1=new MapChartFlow({ID: 'flow1'},socketMock);
             assert.strictEqual(flow1.addRecord(), 133);
         });
         it('returns valid norrisRecordID if record is valid', function() {
-            var flow2=new MapChartFlow({ID: 'flow2'},{_namespace: 'flow2'});
+            var flow2=new MapChartFlow({ID: 'flow2'},socketMock);
             console.dir(flow2);
             console.log(flow2.addRecord({temperature: 2}));
             console.log(flow2.addRecord({temperature: 2}).indexOf('flow2'));
@@ -53,13 +56,13 @@ describe('MapChartFlow', function() {
 
     describe('#deleteRecord', function() {
         it('returns 135 if ID is invalid', function() {
-            var flow1=new MapChartFlow({ID: 'flow1'},{_namespace: 'flow1'});
+            var flow1=new MapChartFlow({ID: 'flow1'},socketMock);
             flow1.addRecord({temperature: 2});
             assert.strictEqual(flow1.deleteRecord(), 135);
             assert.strictEqual(flow1.deleteRecord('flow1asd'), 135);
         });
         it('returns true if ID is valid', function() {
-            var flow1=new MapChartFlow({ID: 'flow1'},{_namespace: 'flow1'});
+            var flow1=new MapChartFlow({ID: 'flow1'},socketMock);
             var ID=flow1.addRecord({temperature: 2});
             assert.strictEqual(flow1.deleteRecord(ID), true);
         });
