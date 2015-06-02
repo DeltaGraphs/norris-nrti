28766a77e749eba6e8188fc615a31ce642adaaf7
MatFur92 <mat_fur_92_pub@yahoo.it>
2015-06-02 19:10:02 +0200

    fixing tests

--- test/businessTier/flow/lineChartFlowSpec.js
+++ test/businessTier/flow/lineChartFlowSpec.js
@@ -42,56 +42,55 @@ describe('LineChartFlow', function() {
     });
 
     it('calls the parent constructor with the params specified', function() {
-        var flow1=new LineChartFlow({ID: 'flow1'},new socketMock);
+        var flow1=new LineChartFlow({ID: 'flow1'},new socketMock());
         assert.strictEqual(flow1._dataLineChartFlow._ID, 'flow1');
         assert.strictEqual(flow1._graphSocket._namespace, 'flow1');
     });
 
     describe('#addRecord', function() {
         it('returns 123 if record is invalid', function() {
-            var flow1=new LineChartFlow({ID: 'flow1'},new socketMock);
+            var flow1=new LineChartFlow({ID: 'flow1'},new socketMock());
             assert.strictEqual(flow1.addRecord(), 123);
         });
         it('returns valid norrisRecordID if record is valid', function() {
-            var flow1=new LineChartFlow({ID: 'flow1'},new socketMock);
+            var flow1=new LineChartFlow({ID: 'flow1'},new socketMock());
             assert.strictEqual(flow1.addRecord({'tempo': 1, 'temperatura': 25}).indexOf('flow1'), 0);
         });
     });
 
     describe('#updateRecord', function() {
         it('returns 123 if record is invalid', function() {
-            var flow1=new LineChartFlow({ID: 'flow1'},new socketMock);
+            var flow1=new LineChartFlow({ID: 'flow1'},new socketMock());
             flow1.addRecord({'tempo': 1, 'temperatura': 25});
             assert.strictEqual(flow1.updateRecord(), 121);
         });
         it('returns 122 if no ID in records', function() {
-            var flow1=new LineChartFlow({ID: 'flow1'},new socketMock);
+            var flow1=new LineChartFlow({ID: 'flow1'},new socketMock());
             var ID=flow1.addRecord({'tempo': 1, 'temperatura': 25});
             assert.strictEqual(flow1.updateRecord(ID+'32442',{'tempo': 4, 'temperatura': 33}), 122);
         });
         it('returns 122 if invalid ID', function() {
-            var flow1=new LineChartFlow({ID: 'flow1'},new socketMock);
+            var flow1=new LineChartFlow({ID: 'flow1'},new socketMock());
             var ID=flow1.addRecord({'tempo': 1, 'temperatura': 25});
             assert.strictEqual(flow1.updateRecord('sad'+ID,{'tempo': 4, 'temperatura': 33}), 122);
         });
         it('returns true with correct update', function() {
-            var flow1=new LineChartFlow({ID: 'flow1'},new socketMock);
+            var flow1=new LineChartFlow({ID: 'flow1'},new socketMock());
             var ID=flow1.addRecord({'tempo': 1, 'temperatura': 25});
             assert.strictEqual(flow1.updateRecord(ID,{'tempo': 4, 'temperatura': 33}), true);
         });
     });
 
     describe('#getReplaceDataJSON', function() {
-        var flow1=new LineChartFlow({ID: 'flow1', name: 'grafico tempo-temperatura',xKey: 'tempo',yKey: 'temperatura'},new socketMock);
+        var flow1=new LineChartFlow({ID: 'flow1', name: 'grafico tempo-temperatura',xKey: 'tempo',yKey: 'temperatura'},new socketMock());
         flow1.addRecord({'tempo': 4, 'temperatura': 4});
         flow1.addRecord({'tempo': 9, 'temperatura': 23});
         flow1.addRecord({'tempo': 6, 'temperatura': 7});
-        console.log('getReplaceDataJSONgetReplaceDataJSONgetReplaceDataJSON');
-        console.log(JSON.stringify(flow1.getReplaceDataJSON()));
+        assert.deepEqual(flow1.getReplaceDataJSON(),{'action':'replaceData','ID':'flow1','records':[{'norrisRecordID':'flow1_1','value':[4,4]},{'norrisRecordID':'flow1_2','value':[9,23]},{'norrisRecordID':'flow1_3','value':[6,7]}]});
     });
 
     describe('#updateProperties', function() {
-        var flow1=new LineChartFlow({ID: 'flow1'},new socketMock);
+        var flow1=new LineChartFlow({ID: 'flow1'},new socketMock());
         flow1.updateProperties({name: 'grafico tempo-temperatura',xKey: 'tempo',yKey: 'temperatura'});
         assert.strictEqual(flow1._dataLineChartFlow._xKey,'tempo');
         assert.strictEqual(flow1._dataLineChartFlow._yKey,'temperatura');
