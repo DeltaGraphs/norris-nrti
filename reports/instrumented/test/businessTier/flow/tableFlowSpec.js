fd077a9343d25f311f6a9667e54c74b486992055
deltagraphsManager <deltagraphs@gmail.com>
2015-06-06 14:52:40 +0000

    Travis build 1405 pushed to gh-pages

--- reports/instrumented/test/businessTier/flow/tableFlowSpec.js
+++ reports/instrumented/test/businessTier/flow/tableFlowSpec.js
@@ -109,12 +109,12 @@ describe('TableFlow', function() {
         it('returns 142 if no ID in records', function() {
             var flow1=new TableFlow({ID: 'flow1'},new socketMock());
             var ID=flow1.addRecord({'tempo': 1, 'temperatura': 25});
-            assert.strictEqual(flow1.updateRecord(ID+'32442',{'tempo': 4, 'temperatura': 33 appearance:[{bg:'#000', text:'#FFF'},{bg:'#000', text:'#FFF'}]}), 142);
+            assert.strictEqual(flow1.updateRecord(ID+'32442',{'tempo': 4, 'temperatura': 33, appearance:[{bg:'#000', text:'#FFF'},{bg:'#000', text:'#FFF'}]}), 142);
         });
         it('returns 142 if invalid ID', function() {
             var flow1=new TableFlow({ID: 'flow1'},new socketMock());
             var ID=flow1.addRecord({'tempo': 1, 'temperatura': 25});
-            assert.strictEqual(flow1.updateRecord('sad'+ID,{'tempo': 4, 'temperatura': 33 appearance:[{bg:'#000', text:'#FFF'},{bg:'#000', text:'#FFF'}]}), 142);
+            assert.strictEqual(flow1.updateRecord('sad'+ID,{'tempo': 4, 'temperatura': 33, appearance:[{bg:'#000', text:'#FFF'},{bg:'#000', text:'#FFF'}]}), 142);
         });
         it('returns true with correct update', function() {
             var mock=new socketMock();
@@ -123,36 +123,36 @@ describe('TableFlow', function() {
                     filters: 'temperature>3',
                     columnKeys: ['col1','col2']
                 }, mock);
-            var ID=flow1.addRecord({temperature: 5, col1:1, col2:'2' appearance:[{bg:'#000', text:'#FFF'},{bg:'#000', text:'#FFF'}]});
+            var ID=flow1.addRecord({temperature: 5, col1:1, col2:'2', appearance:[{bg:'#000', text:'#FFF'},{bg:'#000', text:'#FFF'}]});
             flow1.addRecord({temperature: 6, col1:1, col2:'2'});
-            flow1.updateRecord(ID, {temperature: 0, col1:1, col2:'2' appearance:[{bg:'#000', text:'#FFF'},{bg:'#000', text:'#FFF'}]});
+            flow1.updateRecord(ID, {temperature: 0, col1:1, col2:'2', appearance:[{bg:'#000', text:'#FFF'},{bg:'#000', text:'#FFF'}]});
             assert.strictEqual(mock.p1, 'updateFlowData');
             assert.deepEqual(mock.p2, {
                 action: 'deleteRecord',
                 ID: 'flow1',
                 norrisRecordID: ID
             });
-            flow1.updateRecord(ID, {temperature: 5, col1:1, col2:'2' appearance:[{bg:'#000', text:'#FFF'},{bg:'#000', text:'#FFF'}]});
+            flow1.updateRecord(ID, {temperature: 5, col1:1, col2:'2', appearance:[{bg:'#000', text:'#FFF'},{bg:'#000', text:'#FFF'}]});
             assert.strictEqual(mock.p1, 'updateFlowData');
             assert.deepEqual(mock.p2, {
                 action: 'insertRecords',
                 ID: 'flow1',
                 records: [{
                     norrisRecordID: ID,
-                    value: [1,'2']
+                    value: [1,'2'],
                     appearance:[
                         {bg:'#000', text:'#FFF'},
                         {bg:'#000', text:'#FFA'}
                     ]
                 }]
             });
-            flow1.updateRecord(ID, {temperature: 6, col1:1, col2:'2' appearance:[{bg:'#000', text:'#FFF'},{bg:'#000', text:'#FF'}]});
+            flow1.updateRecord(ID, {temperature: 6, col1:1, col2:'2', appearance:[{bg:'#000', text:'#FFF'},{bg:'#000', text:'#FF'}]});
             assert.strictEqual(mock.p1, 'updateFlowData');
             assert.deepEqual(mock.p2, {
                 action: 'updateRecord',
                 ID: 'flow1',
                 norrisRecordID: ID,
-                value: [1,'2']
+                value: [1,'2'],
                 appearance: undefined
             });
         });
@@ -162,7 +162,7 @@ describe('TableFlow', function() {
         it('returns correct JSON', function() {
             var flow1=new TableFlow({ID: 'flow1', name: 'tabella', columnKeys: ['col1','col2']},new socketMock());
             var ID1=flow1.addRecord({col1: 1, col2:'2', col3:'notThis'});
-            var ID2=flow1.addRecord({col1: 0, col2:0, col3:'notThis' appearance:[{bg:'#000', text:'#FFF'},{bg:'#000', text:'#FFF'}]});
+            var ID2=flow1.addRecord({col1: 0, col2:0, col3:'notThis', appearance:[{bg:'#000', text:'#FFF'},{bg:'#000', text:'#FFF'}]});
             var ID3=flow1.addRecord({col1: 2, col2:1.2});
             assert.deepEqual(flow1.getReplaceDataJSON(),{
                 'action':'replaceData',
@@ -170,7 +170,7 @@ describe('TableFlow', function() {
                 'records':[
                     {'norrisRecordID':ID1,'value':[1,'2'], appearance: undefined},
                     {'norrisRecordID':ID2,'value':[0,0], appearance: [{bg:'#000', text:'#FFF'},{bg:'#000', text:'#FFF'}]},
-                    {'norrisRecordID':ID3,'value':[2,1.2], appearance=undefined}
+                    {'norrisRecordID':ID3,'value':[2,1.2], appearance: undefined}
                 ]
             });
         });
@@ -194,13 +194,13 @@ describe('TableFlow', function() {
                 records: [
                     {norrisRecordID: ID1,
                     value: [1,'2'],
-                    appearance=undefined},
+                    appearance: undefined},
                     {norrisRecordID: ID2,
                     value: [0,0],
-                    appearance=undefined},
+                    appearance: undefined},
                     {norrisRecordID: ID3,
                     value: [2,1.2],
-                    appearance=undefined},
+                    appearance: undefined},
                 ]
             });
             assert.strictEqual(mock.p1[mock.p1.length-1],'updateFlowProp');
