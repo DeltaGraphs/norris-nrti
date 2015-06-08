a89d354ad16955662c502decf871d2d2958756d1
Maria Giovanna Chinellato <mariagiovanna.chinellato1993@gmail.com>
2015-06-09 00:13:38 +0200

    add testing statements fix barchart

--- example/index.js
+++ example/index.js
@@ -335,6 +335,7 @@ var repeatBar=function(){
                 name: 'grafico tempo-pressione',
                 indexKey: 'tempo',
                 valueKey: 'pressione',
+                flowColor: '#33AAFF'
             },[
                 {tempo: 1, pressione: 3},
                 {tempo: 2, pressione: 10},
@@ -348,6 +349,7 @@ var repeatBar=function(){
                 name: 'grafico tempo-umidita',
                 indexKey: 'tempo',
                 valueKey: 'umidita',
+                flowColor: '#22CC55'
             },[
                 {tempo: 1, umidita: 7},
                 {tempo: 2, umidita: 20},
