1ce13b48920fdf5460c5f1e12cb32fcce6d16459
Samuele Zanella <baioke@live.it>
2015-06-09 11:25:34 +0200

    fix barchart test

--- test/businessTier/graph/barChartSpec.js
+++ test/businessTier/graph/barChartSpec.js
@@ -160,9 +160,9 @@ describe('BarChart', function() {
         it('updated properties', function() {
             var mock = new socketMock();
             var barChart=new BarChart({ID: 'dada'}, {_page: 'dssada'}, mock);
-            barChart.updateProperties({title: 'graph one', height: 200, width: 350, enableLegend: true, backgroundColor: '#EEEEEE', legendOnPoint: true, sortable: true, grid: true, barOrientation: 'H', groupingControl:true});
+            barChart.updateProperties({title: 'graph one', height: 200, width: 350, enableLegend: true, backgroundColor: '#EEEEEE', legendOnPoint: true, grid: true, barOrientation: 'H', groupingControl:true});
             assert.strictEqual(mock.p1,'updateGraphProp');
-            assert.deepEqual(mock.p2,{'title':'graph one','height':200,'width':350,'enableLegend':true,'backgroundColor':'#EEEEEE','grid':true,'legendOnPoint':true, 'groupingControl':true,'sortable':true,'barOrientation':'H'});
+            assert.deepEqual(mock.p2,{'title':'graph one','height':200,'width':350,'enableLegend':true,'backgroundColor':'#EEEEEE','grid':true,'legendOnPoint':true, 'groupingControl':true,'barOrientation':'H'});
         });
     });
 
@@ -171,7 +171,7 @@ describe('BarChart', function() {
             var mock=new socketMock();
             var barChart=new BarChart({ID: 'dada'}, {_page: 'dssada'}, mock);
             barChart.createBarChartFlow({ ID:'flow1', name: 'grafico tempo-temperatura', indexKey: 'tempo', valueKey: 'temperatura'});
-            assert.deepEqual(barChart.getProperties(),{'ID':'dada','title':'','type':'BarChart','height':400,'width':500,'enableLegend':false,'legend':{'position':'NE','fontColor':'#000000','backgroundColor':'#FFFFFF'},'grid':false,'xAxis':{'name':'','color':'#000000','maxIndex':null,'minIndex':null,'ticks':10,'scale':'linear'},'yAxis':{'name':'','color':'#000000','maxIndex':null,'minIndex':null,'ticks':10,'scale':'linear'},'backgroundColor':'#FFFFFF','legendOnPoint':false,'sortable':false,'groupingControl':false,'barOrientation':'V', 'headers':[]});
+            assert.deepEqual(barChart.getProperties(),{'ID':'dada','title':'','type':'BarChart','height':400,'width':500,'enableLegend':false,'legend':{'position':'NE','fontColor':'#000000','backgroundColor':'#FFFFFF'},'grid':false,'xAxis':{'name':'','color':'#000000','maxIndex':null,'minIndex':null,'ticks':10,'scale':'linear'},'yAxis':{'name':'','color':'#000000','maxIndex':null,'minIndex':null,'ticks':10,'scale':'linear'},'backgroundColor':'#FFFFFF','legendOnPoint':false,'groupingControl':false,'barOrientation':'V', 'headers':[]});
         });
     });
 
@@ -181,7 +181,7 @@ describe('BarChart', function() {
             var barChart=new BarChart({ID: 'dada'}, {_page: 'dssada'}, mock);
             barChart.createBarChartFlow({ ID:'flow1', name: 'grafico tempo-temperatura', indexKey: 'tempo', valueKey: 'temperatura'}, [{'tempo': 1, 'temperatura': 25}]);
             var ID = barChart.getFlowByID('flow1')._dataBarChartFlow.getRecordByIndex(0).norrisRecordID;
-            assert.deepEqual(barChart.getConfigJSON(),{'properties':{'ID':'dada','title':'','type':'BarChart','height':400,'width':500,'enableLegend':false,'legend':{'position':'NE','fontColor':'#000000','backgroundColor':'#FFFFFF'},'grid':false,'sortable':false,'barOrientation':'V','groupingControl':false,'headers':[],'xAxis':{'name':'','color':'#000000','maxIndex':null,'minIndex':null,'ticks':10,'scale':'linear'},'yAxis':{'name':'','color':'#000000','maxIndex':null,'minIndex':null,'ticks':10,'scale':'linear'},'backgroundColor':'#FFFFFF','legendOnPoint':false,'flows':[{'ID':'flow1','name':'grafico tempo-temperatura','filters':null,'indexKey':'tempo','valueKey':'temperatura','indexFormat':null,'valueFormat':null,'flowColor':null}]},'data':[{'ID':'flow1','records':[{norrisRecordID: ID, value: [1,25]}]}]});
+            assert.deepEqual(barChart.getConfigJSON(),{'properties':{'ID':'dada','title':'','type':'BarChart','height':400,'width':500,'enableLegend':false,'legend':{'position':'NE','fontColor':'#000000','backgroundColor':'#FFFFFF'},'grid':false,'barOrientation':'V','groupingControl':false,'headers':[],'xAxis':{'name':'','color':'#000000','maxIndex':null,'minIndex':null,'ticks':10,'scale':'linear'},'yAxis':{'name':'','color':'#000000','maxIndex':null,'minIndex':null,'ticks':10,'scale':'linear'},'backgroundColor':'#FFFFFF','legendOnPoint':false,'flows':[{'ID':'flow1','name':'grafico tempo-temperatura','filters':null,'indexKey':'tempo','valueKey':'temperatura','indexFormat':null,'valueFormat':null,'flowColor':null}]},'data':[{'ID':'flow1','records':[{norrisRecordID: ID, value: [1,25]}]}]});
         });
     });
 });
