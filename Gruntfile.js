7db826581aed7ed9dc992895802365ba7ff35d57
MatFur92 <mat_fur_92_pub@yahoo.it>
2015-06-10 10:21:11 +0200

    add complete css and js

--- Gruntfile.js
+++ Gruntfile.js
@@ -9,6 +9,10 @@ module.exports = function(grunt) {
                 src: ['frontend/app/View/**/*.css'], // array of folders
                 dest: 'frontend/app/dist/norris-nrti.css'
             },
+            cssComplete: {
+                src: ['frontend/app/View/**/*.css','bower_components/bootstrap/dist/css/bootstrap.min.css'], // array of folders
+                dest: 'frontend/app/dist/norris-nrti.complete.css'
+            },
             js: {
                 src: [ // array of folders
                     'frontend/app/norris-nrti.js',
@@ -17,19 +21,37 @@ module.exports = function(grunt) {
                     'frontend/app/View/**/*.js'
                 ],
                 dest: 'frontend/app/dist/norris-nrti.js'
+            },
+            jsComplete: {
+                src: [ // array of folders
+                    'frontend/app/norris-nrti.js',
+                    'frontend/app/Model/**/*.js',
+                    'frontend/app/Controller/**/*.js',
+                    'frontend/app/View/**/*.js',
+                    'frontend/app/bower_components/sio-client/socket.io.js',
+                    'frontend/app/bower_components/angular/angular.min.js',
+                    'frontend/app/bower_components/jquery/dist/jquery.min.js',
+                    'frontend/app/bower_components/bootstrap/dist/js/bootstrap.min.js',
+                    'frontend/app/bower_components/angular-route/angular-route.js',
+                    'frontend/app/bower_components/d3/d3.min.js',
+                    'frontend/app/bower_components/angular-smart-table/dist/smart-table.min.js',
+                ],
+                dest: 'frontend/app/dist/norris-nrti.complete.js'
             }
         },
         cssmin: {
             dist: {
                 files: {
-                    'frontend/app/dist/norris-nrti.min.css': [ 'frontend/app/dist/norris-nrti.css' ]
+                    'frontend/app/dist/norris-nrti.min.css': [ 'frontend/app/dist/norris-nrti.css' ],
+                    'frontend/app/dist/norris-nrti.complete.min.css': [ 'frontend/app/dist/norris-nrti.complete.css' ]
                 }
             }
         },
         uglify: {
             build: {
                 files: {
-                    'frontend/app/dist/norris-nrti.min.js': ['frontend/app/dist/norris-nrti.js']
+                    'frontend/app/dist/norris-nrti.min.js': ['frontend/app/dist/norris-nrti.js'],
+                    'frontend/app/dist/norris-nrti.complete.min.js': ['frontend/app/dist/norris-nrti.complete.js']
                 }
             }
         }
