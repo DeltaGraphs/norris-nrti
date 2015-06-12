a7e239f4e53b2e361a1cb7290159742d82591331
deltagraphsManager <deltagraphs@gmail.com>
2015-06-12 17:07:38 +0000

    Travis build 1588 pushed to gh-pages

---
+++ reports-frontend/metrics/files/test_unit_Model_UrlProviderSpec_js/report.history.js
@@ -0,0 +1,1 @@
+__history = [{"date":"Fri, 12 Jun 2015 17:07:37 GMT","sloc":67,"lloc":25,"functions":10,"deliveredBugs":0.18833333333333332,"maintainability":83.6836092268602,"lintErrors":17,"difficulty":7.269230769230768}]
