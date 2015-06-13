4b39c3b3f4d62fa28e5147839eca7faa0b518c3e
Maria Giovanna Chinellato <mariagiovanna.chinellato1993@gmail.com>
2015-06-13 10:44:36 +0200

    controller test: some fixes

--- frontend/test/unit/Controller/MapChartControllerSpec.js
+++ frontend/test/unit/Controller/MapChartControllerSpec.js
@@ -26,29 +26,31 @@ describe('MapChartController', function(){
 		this.events = {};
 
 		// Receive Events
-		this.on = function(eventName, callback){
-			if(!this.events[eventName]){
-				this.events[eventName] = [];
-			}
-			this.events[eventName].push(callback);
-		};
+		return {
+			on : function(eventName, callback){
+				if(!this.events[eventName]){
+					this.events[eventName] = [];
+				}
+				this.events[eventName].push(callback);
+			},
 
 		// Send Events
-		this.emit = function(eventName, data, emitCallback){
-			if(this.events[eventName]){
-				angular.forEach(this.events[eventName], function(callback){
-					$rootScope.$apply(function() {
-						callback(data);
+			emit: function(eventName, data, emitCallback){
+				if(this.events[eventName]){
+					angular.forEach(this.events[eventName], function(callback){
+						$rootScope.$apply(function() {
+							callback(data);
+						});
 					});
-				});
-			}
-			if(emitCallback){
-				emitCallback();
+				}
+				if(emitCallback){
+					emitCallback();
+				}
 			}
 		};
 
 	};
-
+	
     beforeEach(inject(function($rootScope, $controller){
     	scope = $rootScope.$new();
         controller = $controller('MapChartController', { $scope : scope, socket : socket });
--- frontend/test/unit/Controller/RootControllerSpec.js
+++ frontend/test/unit/Controller/RootControllerSpec.js
@@ -14,7 +14,6 @@
 */
 
 describe('RootController', function(){
-	'use strict';
 
 	var scope;
 
