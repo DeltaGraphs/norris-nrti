b317d857794d1456d6c72ef61df8c7ab417cfe17
Maria Giovanna Chinellato <mariagiovanna.chinellato1993@gmail.com>
2015-06-23 23:38:18 +0200

    some fixes

--- frontend/test/unit/Controller/MapChartControllerSpec.js
+++ frontend/test/unit/Controller/MapChartControllerSpec.js
@@ -20,265 +20,178 @@
 
 describe('MapChartController', function(){
 
-	beforeEach(angular.mock.module('norris-nrti'));
+  beforeEach(angular.mock.module('norris-nrti'));
+  beforeEach(angular.mock,module('norris-nrti-mock-socket'));
 
-	var scope;
-	var controller;
-	var socket;
-	
-    /*beforeEach(inject(function($rootScope, $controller){
-    	//var socketURL = 'http://0.0.0.0:5000/namespace';
-        var options ={
-            transports: ['websocket'],
-            'force new connection': true
-        };
-    	socket = io.connect('http://norris-nrti-dev.herokuapp.com/page1/map1',options);
-    	scope = $rootScope.$new();
-        controller = $controller('MapChartController', { $scope : scope, socket : socket });
-    }));
+  var scope;
+  var controller;
+  var socket;
 
-    describe('Constructor', function(){
-    	it('controller is defined', function() {
-			expect(controller).toBeDefined();
-		});
-
-    	it('controller is defined', function() {
-			expect(socket).toBeDefined();
-		});
-
-	    it('scope.mapChart is defined', function() {
-			expect(scope.mapChart).toBeDefined();
-		});
-    });
-
-	describe('listenOnEvent', function(){
-    	
-	    it('configGraph works fine', function(){
-			socket.on('configGraph', function({
-				'properties':{
-					'ID':'map1',
-					'title':'APS',
-					'type':'MapChart',
-					'height':600,
-					'width':1000,
-					'flows':[
-						{
-							'ID':'flow1',
-							'name':'linea 22',
-							'filters':null,
-							'longitudeKey':'2',
-							'latitudeKey':'1',
-							'objectKey':'0',
-							'longitudeFormat':'coordinates',
-							'latitudeFormat':'coordinates',
-							'marker':{
-								'type':'shape',
-								'shape':'bus',
-								'color':'#FFC4F6'
-							}
-						}
-					]
-				}
-			}));
-			expect(scope.mapChart.getTitle()).toEqual('APS');
-			expect(scope.mapChart.getHeight()).toEqual(600);
-			expect(scope.mapChart.getWidth()).toEqual(1000);
-			expect(scope.mapChart.getFlowList().length).toEqual(1);
-		});
-
-	});*/
-
-	/*var scope;
-	var controller;
-	var notify;
-
-    beforeEach(inject(function($rootScope, $controller, _socketFactory_){
-    	scope = $rootScope.$new();
-    	notify = _socketFactory_;
-        controller = $controller('MapChartController', { $scope : scope });
+    beforeEach(inject(function($rootScope, $controller, $injector){
+		scope = $rootScope.$new();
+		socket = $injector('SocketServicesFactory');
+		controller = $controller('MapChartController', { $scope : scope });
     }));
 
     it('scope.mapChart is defined', function() {
-		expect(scope.mapChart).toBeDefined();
-	});
+    expect(scope.mapChart).toBeDefined();
+  });
 
-	it('scope.mapChart is defined', function() {
-		expect(notify).toBeDefined();
-	});
+  it('controller is defined', function() {
+    expect(controller).toBeDefined();
+  });
 
-    deeescribe('listenOnEvent', function(){
-    	
-	    it('configGraph works fine', function(){
-			notify.receive('configGraph', {
-				'properties':{
-					'ID':'map1',
-					'title':'APS',
-					'type':'MapChart',
-					'height':600,
-					'width':1000,
-					'flows':[
-						{
-							'ID':'flow1',
-							'name':'linea 22',
-							'filters':null,
-							'longitudeKey':'2',
-							'latitudeKey':'1',
-							'objectKey':'0',
-							'longitudeFormat':'coordinates',
-							'latitudeFormat':'coordinates',
-							'marker':{
-								'type':'shape',
-								'shape':'bus',
-								'color':'#FFC4F6'
-							}
-						}
-					]
-				}
-			});
-			expect(scope.mapChart.getTitle()).toEqual('APS');
-			expect(scope.mapChart.getHeight()).toEqual(600);
-			expect(scope.mapChart.getWidth()).toEqual(1000);
-			expect(scope.mapChart.getFlowList().length).toEqual(1);
-		});
-
-		it('updateGraphProp works fine', function(){
-			notify.receive('updateGraphProp', {
-				'height':1000,
-				'width':600
-			});
-			expect(scope.mapChart.getHeight()).toEqual(1000);
-			expect(scope.mapChart.getWidth()).toEqual(600);
-		});
-
+    describe('#listenOnEvent', function(){
+      
 		it('configGraph works fine', function(){
-			socket.on('configGraph', {
-				'properties':{
-					'ID':'map1',
-					'title':'APS',
-					'type':'MapChart',
-					'height':600,
-					'width':1000,
-					'flows':[
-						{
-							'ID':'flow1',
-							'name':'linea 22',
-							'filters':null,
-							'longitudeKey':'2',
-							'latitudeKey':'1',
-							'objectKey':'0',
-							'longitudeFormat':'coordinates',
-							'latitudeFormat':'coordinates',
-							'marker':{
-								'type':'shape',
-								'shape':'bus',
-								'color':'#FFC4F6'
-							},
-						}
-					]
-				}
-			});
-			expect(scope.mapChart.getTitle()).toEqual('APS');
-			expect(scope.mapChart.getHeight()).toEqual(600);
-			expect(scope.mapChart.getWidth()).toEqual(1000);
-			expect(scope.mapChart.getFlowList().length).toEqual(1);
+		var configGraph = false;
+		socket.on('configGraph', function(){
+			configGraph = true;
 		});
+		expect(configGraph).toEqual(true);
+		//expect(scope.mapChart.getHeight()).toEqual(600);
+		//expect(scope.mapChart.getWidth()).toEqual(1000);
+		//expect(scope.mapChart.getFlowList().length).toEqual(1);
+    });
 
-		it('configGraph works fine', function(){
-			socket.on('configGraph', {
-				'properties':{
-					'ID':'map1',
-					'title':'APS',
-					'type':'MapChart',
-					'height':600,
-					'width':1000,
-					'flows':[
-						{
-							'ID':'flow1',
-							'name':'linea 22',
-							'filters':null,
-							'longitudeKey':'2',
-							'latitudeKey':'1',
-							'objectKey':'0',
-							'longitudeFormat':'coordinates',
-							'latitudeFormat':'coordinates',
-							'marker':{
-								'type':'shape',
-								'shape':'bus',
-								'color':'#FFC4F6'
-							},
-						}
-					]
-				}
-			});
-			expect(scope.mapChart.getTitle()).toEqual('APS');
-			expect(scope.mapChart.getHeight()).toEqual(600);
-			expect(scope.mapChart.getWidth()).toEqual(1000);
-			expect(scope.mapChart.getFlowList().length).toEqual(1);
-		});
+    /*it('updateGraphProp works fine', function(){
+      notify.receive('updateGraphProp', {
+        'height':1000,
+        'width':600
+      });
+      expect(scope.mapChart.getHeight()).toEqual(1000);
+      expect(scope.mapChart.getWidth()).toEqual(600);
+    });
 
-		it('configGraph works fine', function(){
-			socket.on('configGraph', {
-				'properties':{
-					'ID':'map1',
-					'title':'APS',
-					'type':'MapChart',
-					'height':600,
-					'width':1000,
-					'flows':[
-						{
-							'ID':'flow1',
-							'name':'linea 22',
-							'filters':null,
-							'longitudeKey':'2',
-							'latitudeKey':'1',
-							'objectKey':'0',
-							'longitudeFormat':'coordinates',
-							'latitudeFormat':'coordinates',
-							'marker':{
-								'type':'shape',
-								'shape':'bus',
-								'color':'#FFC4F6'
-							},
-						}
-					]
-				}
-			});
-			expect(scope.mapChart.getTitle()).toEqual('APS');
-			expect(scope.mapChart.getHeight()).toEqual(600);
-			expect(scope.mapChart.getWidth()).toEqual(1000);
-			expect(scope.mapChart.getFlowList().length).toEqual(1);
-		});
+    it('configGraph works fine', function(){
+      socket.on('configGraph', {
+        'properties':{
+          'ID':'map1',
+          'title':'APS',
+          'type':'MapChart',
+          'height':600,
+          'width':1000,
+          'flows':[
+            {
+              'ID':'flow1',
+              'name':'linea 22',
+              'filters':null,
+              'longitudeKey':'2',
+              'latitudeKey':'1',
+              'objectKey':'0',
+              'longitudeFormat':'coordinates',
+              'latitudeFormat':'coordinates',
+              'marker':{
+                'type':'shape',
+                'shape':'bus',
+                'color':'#FFC4F6'
+              },
+            }
+          ]
+        }
+      });
+      expect(scope.mapChart.getTitle()).toEqual('APS');
+      expect(scope.mapChart.getHeight()).toEqual(600);
+      expect(scope.mapChart.getWidth()).toEqual(1000);
+      expect(scope.mapChart.getFlowList().length).toEqual(1);
+    });
+
+    it('configGraph works fine', function(){
+      socket.on('configGraph', {
+        'properties':{
+          'ID':'map1',
+          'title':'APS',
+          'type':'MapChart',
+          'height':600,
+          'width':1000,
+          'flows':[
+            {
+              'ID':'flow1',
+              'name':'linea 22',
+              'filters':null,
+              'longitudeKey':'2',
+              'latitudeKey':'1',
+              'objectKey':'0',
+              'longitudeFormat':'coordinates',
+              'latitudeFormat':'coordinates',
+              'marker':{
+                'type':'shape',
+                'shape':'bus',
+                'color':'#FFC4F6'
+              },
+            }
+          ]
+        }
+      });
+      expect(scope.mapChart.getTitle()).toEqual('APS');
+      expect(scope.mapChart.getHeight()).toEqual(600);
+      expect(scope.mapChart.getWidth()).toEqual(1000);
+      expect(scope.mapChart.getFlowList().length).toEqual(1);
+    });
+
+    it('configGraph works fine', function(){
+      socket.on('configGraph', {
+        'properties':{
+          'ID':'map1',
+          'title':'APS',
+          'type':'MapChart',
+          'height':600,
+          'width':1000,
+          'flows':[
+            {
+              'ID':'flow1',
+              'name':'linea 22',
+              'filters':null,
+              'longitudeKey':'2',
+              'latitudeKey':'1',
+              'objectKey':'0',
+              'longitudeFormat':'coordinates',
+              'latitudeFormat':'coordinates',
+              'marker':{
+                'type':'shape',
+                'shape':'bus',
+                'color':'#FFC4F6'
+              },
+            }
+          ]
+        }
+      });
+      expect(scope.mapChart.getTitle()).toEqual('APS');
+      expect(scope.mapChart.getHeight()).toEqual(600);
+      expect(scope.mapChart.getWidth()).toEqual(1000);
+      expect(scope.mapChart.getFlowList().length).toEqual(1);
+    });*/
     });
     
-	//deeescribe('socketConnection', function(){
+  //deeescribe('socketConnection', function(){
 
-	//});
+  //});
 
-	deeescribe('listenOnEvent', function(){
+  /*deeescribe('listenOnEvent', function(){
 
-		var data = [
-			{
-				'ID' : '1',
-				'records' : [{ 'NorrisRecordID' : '234321', 'value' : [0,1]},{}]
-			}
-		];
+    var data = [
+      {
+        'ID' : '1',
+        'records' : [{ 'NorrisRecordID' : '234321', 'value' : [0,1]},{}]
+      }
+    ];
 
-		beforeEach(function(){
-			scope.mapChart = MapChartFactory.build(json);
-		});
-		
-		it ('listenOnEvent works fine', function(){
-			controller('MapChartController', {$scope : scope});
-			expect(scope.mapChart.getTitle()).toEqual('graficonuovo');
-			notify.receive('configGraph',{
-				'properties' : {
-					'title' : 'titolocambiato'
-				},
-				'data' : data
-			});
-			expect(scope.mapChart.getTitle()).toEqual('titolocambiato');
-			expect(scope.mapChart.getFlowList()[0].flow.getData()[0].value[0]).toEqual(0);
-		});
-	});*/
-	
+    beforeEach(function(){
+      scope.mapChart = MapChartFactory.build(json);
+    });
+    
+    it ('listenOnEvent works fine', function(){
+      controller('MapChartController', {$scope : scope});
+      expect(scope.mapChart.getTitle()).toEqual('graficonuovo');
+      notify.receive('configGraph',{
+        'properties' : {
+          'title' : 'titolocambiato'
+        },
+        'data' : data
+      });
+      expect(scope.mapChart.getTitle()).toEqual('titolocambiato');
+      expect(scope.mapChart.getFlowList()[0].flow.getData()[0].value[0]).toEqual(0);
+    });
+  });*/
+  
 });
---
+++ frontend/test/unit/Controller/SocketServices.js
@@ -0,0 +1,46 @@
+/*jshint node: true */
+'use strict';
+
+/*
+* Name :  SocketServices.js
+* Module : UnitTest
+* Location : /frontend/test/unit/Controller
+*
+* History :
+* Version       Date        Programmer                  Description
+* =================================================================================================
+* 0.1.0         2015-06-23  Maria Giovanna Chinellato   Add all attributes and all methods
+*
+* 0.0.1         2015-06-23  Maria Giovanna Chinellato   Initial code      
+* =================================================================================================
+*
+*/
+
+angular.module('norris-nrti-mock-socket')
+.factory('SocketServicesFactory', function ($rootScope) {
+
+	this.events = {};
+
+	// Receive Events
+	this.on = function(eventName, callback){
+		if(!this.events[eventName]){
+			this.events[eventName] = [];
+		}
+		this.events[eventName].push(callback);
+	};
+
+	// Send Events
+	this.emit = function(eventName, data, emitCallback){
+		if(this.events[eventName]){
+			angular.forEach(this.events[eventName], function(callback){
+				$rootScope.$apply(function() {
+					callback(data);
+				});
+			});
+		}
+		if(emitCallback){
+			emitCallback();
+		}
+	};
+
+});
