dfde2f08162b57911907cd4fc1c40cb2599216c5
Maria Giovanna Chinellato <mariagiovanna.chinellato1993@gmail.com>
2015-06-23 15:37:08 +0200

    some fixes

--- frontend/test/unit/Controller/MapChartControllerSpec.js
+++ frontend/test/unit/Controller/MapChartControllerSpec.js
@@ -9,6 +9,8 @@
 * History :
 * Version       Date        Programmer                  Description
 * =================================================================================================
+* 0.1.0         2015-05-25  Maria Giovanna Chinellato   fix configGraph
+*
 * 0.1.0         2015-05-25  Maria Giovanna Chinellato   Add all attributes and all methods
 *
 * 0.0.1         2015-05-25  Maria Giovanna Chinellato   Initial code      
