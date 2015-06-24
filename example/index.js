9d8f91ea0b181e8d081ee074fd464d46a08107ae
MatFur92 <mat_fur_92_pub@yahoo.it>
2015-06-24 19:15:55 +0200

    try to fix 404 problem

--- example/index.js
+++ example/index.js
@@ -565,66 +565,19 @@ app.get('/', function (req, res) {
 });
 */
 
-
-app.use(function(req, res, next){
-  res.status(404);
-  
-  // respond with html page
-  if (req.accepts('html')) {
-    res.render('404', { url: req.url });
-    return;
-  }
-
-  // respond with json
-  if (req.accepts('json')) {
-    res.send({ error: 'Not found' });
-    return;
-  }
-
-  // default to plain-text. send()
-  res.type('txt').send('Not found');
-});
-
-// error-handling middleware, take the same form
-// as regular middleware, however they require an
-// arity of 4, aka the signature (err, req, res, next).
-// when connect has an error, it will invoke ONLY error-handling
-// middleware.
-
-// If we were to next() here any remaining non-error-handling
-// middleware would then be executed, or if we next(err) to
-// continue passing the error, only error-handling middleware
-// would remain being executed, however here
-// we simply respond with an error page.
-
-app.use(function(err, req, res, next){
-    // we may use properties of the error object
-    // here and next(err) appropriately, or if
-    // we possibly recovered from the error, simply next().
-    res.status(err.status || 500);
-    res.render('500', { error: err });
-});
-
-// Routes
-
-
-app.get('/404', function(req, res, next){
-    // trigger a 404 since no other middleware
-    // will match /404 after this one, and we're not
-    // responding here
-    next();
-});
-
-app.get('/403', function(req, res, next){
-    // trigger a 403 error
-    var err = new Error('not allowed!');
-    err.status = 403;
+app.get('*', function(req, res, next) {
+    var err = new Error();
+    err.status = 404;
     next(err);
 });
+ 
+// handling 404 errors
+app.use(function(err, req, res, next) {
+    if(err.status !== 404) {
+        return next();
+    }
 
-app.get('/500', function(req, res, next){
-    // trigger a generic (500) error
-    next(new Error('keyboard cat!'));
+    res.send(err.message || '** no unicorns here **');
 });
 
 
