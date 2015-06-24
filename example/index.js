4d350c8d85acd3f11f46a38370f948f65a687f64
MatFur92 <mat_fur_92_pub@yahoo.it>
2015-06-24 19:11:50 +0200

    try to have a 404 working

--- example/index.js
+++ example/index.js
@@ -560,9 +560,73 @@ app.get('/', function (req, res) {
 });*/
 
 //The 404 Route (ALWAYS Keep this as the last route)
-app.get('*', function(req, res){
+/*app.get('*', function(req, res){
     res.send('what???', 404);
 });
+*/
+
+
+app.use(function(req, res, next){
+  res.status(404);
+  
+  // respond with html page
+  if (req.accepts('html')) {
+    res.render('404', { url: req.url });
+    return;
+  }
+
+  // respond with json
+  if (req.accepts('json')) {
+    res.send({ error: 'Not found' });
+    return;
+  }
+
+  // default to plain-text. send()
+  res.type('txt').send('Not found');
+});
+
+// error-handling middleware, take the same form
+// as regular middleware, however they require an
+// arity of 4, aka the signature (err, req, res, next).
+// when connect has an error, it will invoke ONLY error-handling
+// middleware.
+
+// If we were to next() here any remaining non-error-handling
+// middleware would then be executed, or if we next(err) to
+// continue passing the error, only error-handling middleware
+// would remain being executed, however here
+// we simply respond with an error page.
+
+app.use(function(err, req, res, next){
+    // we may use properties of the error object
+    // here and next(err) appropriately, or if
+    // we possibly recovered from the error, simply next().
+    res.status(err.status || 500);
+    res.render('500', { error: err });
+});
+
+// Routes
+
+
+app.get('/404', function(req, res, next){
+    // trigger a 404 since no other middleware
+    // will match /404 after this one, and we're not
+    // responding here
+    next();
+});
+
+app.get('/403', function(req, res, next){
+    // trigger a 403 error
+    var err = new Error('not allowed!');
+    err.status = 403;
+    next(err);
+});
+
+app.get('/500', function(req, res, next){
+    // trigger a generic (500) error
+    next(new Error('keyboard cat!'));
+});
+
 
 //app.listen(3000); // si mette in ascolto su http://127.0.0.1:3000/
 var port = process.env.PORT || 3000;
