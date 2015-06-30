45eaeeafee540d4d14a1176e335ce0ca18d638ea
Filippo <filippo.pado@gmail.com>
2015-07-01 01:16:39 +0200

    test

--- systemTest/TS13.js
+++ systemTest/TS13.js
@@ -12,7 +12,7 @@
 *	FBDE3.3.8.2 		Il framework deve dare la possibilità all'utente sviluppatore di modificare il numero massimo di elementi visualizzati nella table.
 *	FBDE3.3.8.3 		Il framework deve dare la possibilità all'utente sviluppatore di modificare la posizione di aggiunta dei dati in una table.
 *	FBOB3.3.8.3.1 		Il framework deve dare la possibilità all'utente sviluppatore di impostare la posizione di inserimento in testa dei dati in una table.
-*	FBDE3.3.8.3.2 		Il framework deve dare la possibilità all'utente sviluppatore di impostare la posizione di inserimento in coda dei dati in una table.
+*	FBOB3.3.8.3.2 		Il framework deve dare la possibilità all'utente sviluppatore di impostare la posizione di inserimento in coda dei dati in una table.
 *	FBDE3.3.8.4 		Il framework deve dare la possibilità all'utente sviluppatore di modificare l'ordinamento dei dati in una table, in base ad una colonna.
 *	FBDE3.3.8.4.1 		Il framework deve dare la possibilità all'utente sviluppatore di modificare l'ordinamento dei dati in una table, in base a più colonne.
 *	FBDE3.3.8.5 		Il framework deve consentire all'utente sviluppatore di modificare la griglia nella table.
@@ -76,7 +76,7 @@ var TS=function(table){
 			    addRowOn: 'bottom',
 			    headers: ['CHANGE1', 'CHANGE2', 'CHANGE3'],
 			    sort: {
-			        column: ['CHANGE-1'],
+			        column: ['CHANGE1'],
 			        ordering: ['DESC']
 			    },
 			    appearance: {}
