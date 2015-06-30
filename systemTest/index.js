8d67662422c2ab75adc67e4dee46b2fa193876e4
Maria Giovanna Chinellato <mariagiovanna.chinellato1993@gmail.com>
2015-06-30 02:17:36 +0200

    fix index.js

--- systemTest/index.js
+++ systemTest/index.js
@@ -32,8 +32,8 @@ var page2=Helper.newPage(norris, 'page2', 'Pagina aggiornamento grafici');
 
 //var barChart=Helper.newBarChart(page2, 'Test aggiornamento dati Bar Chart');
 var lineChart=Helper.newLineChart(page2, 'Test aggiornamento dati Line Chart');
-//var mapChart=Helper.newMapChart(page2, 'Test aggiornamento dati Map Chart');
-var table=Helper.newTable(page2, 'Test aggiornamento dati Table');
+var mapChart=Helper.newMapChart(page2, 'Test aggiornamento dati Map Chart');
+//var table=Helper.newTable(page2, 'Test aggiornamento dati Table');
 
 var TS1 = require('./TS1.js');
 var TS2 = require('./TS2.js');
